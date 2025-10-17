09a47bd7c55d881073aae2d6a764437727b8eda1
tanzeela-fe <tanzeela.fe.tafsol@gmail.com>
2025-10-17 13:34:31 +0500

    Add react-modern-drawer and enhance styles
    - Integrated react-modern-drawer version 1.4.0 into package.json and package-lock.json for improved drawer functionality.
    - Updated layout.js to include styles for the new drawer component.
    - Enhanced typography.css with new responsive font size variables for better adaptability.
    - Added blink animation to various components for improved visual effects.
    - Refactored Header component to handle navigation more robustly and added cursor pointer for better UX.
    - Updated app data to include icons for navigation items, enhancing visual consistency.

--- src/components/organisms/WebsiteHeader/index.jsx
+++ src/components/organisms/WebsiteHeader/index.jsx
@@ -1,32 +1,21 @@
 "use client";
-
-// import RenderToast from "@/components/molecules/RenderToast";
-// import { useMobileViewHook } from "@/resources/hooks/useMobileViewHook";
-// import { clearAllCookies } from "@/resources/utils/cookie";
-// import { signOutRequest } from "@/store/auth/authSlice";
-import { usePathname, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-// import MobileHeader from "./MobileHeader/MobileHeader";
+import { useMobileViewHook } from "@/resources/hooks/useMobileViewHook";
+import MobileDrawer from "@/components/molecules/MobileDrawer";
 import Header from "@/components/molecules/Header/Header";
 
 const WebsiteHeader = () => {
-  // const router = useRouter();
-  // const dispatch = useDispatch();
-  // const pathName = usePathname();
-
-  // const { isMobile } = useMobileViewHook(992);
+  const { isMobile } = useMobileViewHook(800);
 
 
   return (
     <>
-      {/* {isMobile ? (
-        <MobileHeader/>
+      {isMobile ? (
+        <MobileDrawer/>
       ) : (
-        <> */}
+        <> 
           <Header />
-        {/* </>
-      )} */}
+        </>
+      )}
     </>
   );
 };
